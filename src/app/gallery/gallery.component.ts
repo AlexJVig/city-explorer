b9e4efb136db3c4f65c65493c0f9cab725eaed19
Alex <alzombii@gmail.com>
2021-06-08 13:30:23 +0300

    Gallery displays single images

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { createClient } from 'pexels';
+
+const client = createClient('563492ad6f9170000100000122bbd5b61dd3428b85b45b0b0053b30a');
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
-export class GalleryComponent implements OnInit {
+export class GalleryComponent implements OnChanges {
+
+  @Input() galleryInput = '';
+
+  photoResource: any = {};
+
+  currentImagePath = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    let query = changes.galleryInput.currentValue;
+
+    if (query !== '') {
+      client.photos.search({ query, per_page: 1 }).then(photos => {
+        this.photoResource = photos;
+        console.log(this.photoResource);
+        this.switchPhoto(0);
+      });
+    }
+  }
+
+  switchPhoto(photoIndex: number) {
+    photoIndex = photoIndex;
+    this.currentImagePath = this.photoResource.photos[photoIndex].src.large;
   }
 
 }
